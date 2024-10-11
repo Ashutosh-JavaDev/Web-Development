46be1e2900397fb13afe5e587f4a641e3acc2859
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-11 17:52:36 +0530

    Project

--- Projects/JavaScript/Switch.js
+++ Projects/JavaScript/Switch.js
@@ -4,6 +4,10 @@ switch (Prefrence) {
   case "Coca-Cola":
     text = "Coca-Cola!You have Great Choice";
     break;
-    case "Sprite":
-        text="Oh! it's my Favorite too";
+  case "Sprite":
+    text = "Oh! it's my Favorite too";
+    break;
+  default:
+    text = "Choose Wisely";
+    break;
 }
