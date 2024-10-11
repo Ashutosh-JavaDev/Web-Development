a14aee5762c8faadfab2f89fcc92c478e01c8b3f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-11 17:50:19 +0530

    Project

--- Projects/JavaScript/Switch.js
+++ Projects/JavaScript/Switch.js
@@ -1,2 +1,7 @@
 let text;
-let Prefrence=prompt("What is  Your Favourite Drink");
+let Prefrence = prompt("What is  Your Favourite Drink");
+switch (Prefrence) {
+  case "Coca-Cola":
+    text = "Coca-Cola!You have Great Choice";
+    break;
+}
