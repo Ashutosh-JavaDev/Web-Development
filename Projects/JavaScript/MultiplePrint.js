e0e21ee7ee1e3b97c867df7c126ddcda1fa87f44
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-12 17:37:59 +0530

    Project

--- Projects/JavaScript/MultiplePrint.js
+++ Projects/JavaScript/MultiplePrint.js
@@ -1,18 +1,18 @@
-let  count1=0,count2=0,count3=0;
-for(let i=1;i<=100;i++){
- if(i%3==0){
+let count1 = 0,
+  count2 = 0,
+  count3 = 0;
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0) {
     console.log("Fizz");
     count1++;
- }   
- else if(i%5==0){
+  } else if (i % 5 == 0) {
     console.log("Buzz");
     count2++;
- }
- else if(i%3==0 && i%5==0){
+  } else if (i % 3 == 0 && i % 5 == 0) {
     console.log("Muzz");
     count3++;
- }
+  }
 }
-console.log("Count 1 :",count1);
-console.log("Count 2 :",count2);
-console.log("Count 3 :",count3);
+console.log("Count 1 :", count1);
+console.log("Count 2 :", count2);
+console.log("Count 3 :", count3);
