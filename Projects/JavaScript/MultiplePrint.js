fe76f29ace283031c3a9dc5de234b42dbec48e8a
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-12 17:37:06 +0530

    Project

--- Projects/JavaScript/MultiplePrint.js
+++ Projects/JavaScript/MultiplePrint.js
@@ -1,11 +1,15 @@
+let  count1=0,count2=0,count3=0;
 for(let i=1;i<=100;i++){
  if(i%3==0){
     console.log("Fizz");
+    count1++;
  }   
  else if(i%5==0){
     console.log("Buzz");
+    count2++;
  }
  else if(i%3==0 && i%5==0){
     console.log("Muzz");
+    count3++;
  }
 }
