8c780a79f03dc9d7dfdd5a5d2c07d744f7e4898a
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-12 17:25:53 +0530

    Project

--- Projects/JavaScript/Grade.js
+++ Projects/JavaScript/Grade.js
@@ -1,19 +1,14 @@
-let Marks=prompt("Enter the Marks");
-if(Marks>90){
-    console.log("A");
-}
-else if(Marks>80){
-    console.log("B");
-}
-else if(Marks>70){
-    console.log("C");
-}
-else if(Marks>60){
-    console.log("D");
-}
-else if(Marks>40){
-    console.log("E");
-}
-else{
- console.log("Better Luck Next Time....");   
+let Marks = prompt("Enter the Marks");
+if (Marks > 90) {
+  console.log("A");
+} else if (Marks > 80) {
+  console.log("B");
+} else if (Marks > 70) {
+  console.log("C");
+} else if (Marks > 60) {
+  console.log("D");
+} else if (Marks > 40) {
+  console.log("E");
+} else {
+  console.log("Better Luck Next Time....");
 }
