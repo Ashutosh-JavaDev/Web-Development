7f55aa772b54fa824baddecb58fb47526681c729
Ashutosh Ayush <jbnashu297@gmail.com>
2024-10-13 16:40:01 +0530

    Project

--- Projects/JavaScript/Sorting.js
+++ Projects/JavaScript/Sorting.js
@@ -1,4 +1,11 @@
 let arr=[12,312,2443,42,23,112];
-
-arr.sort((a,b)=>a-b);
+for(let i=0;i<arr.length-1;i++){
+    for(let j=0;j<arr.length-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            let temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+        }
+    }
+}
 console.log(arr);
